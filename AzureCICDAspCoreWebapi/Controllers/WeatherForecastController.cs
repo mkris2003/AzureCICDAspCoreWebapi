18f4194a3a7c8c5ac13cadcccad7b4a0933df58f
Murali Konanki <muralikrishna.konanki@gmail.com>
2020-05-01 20:45:01 -0500

    Added new method

--- AzureCICDAspCoreWebapi/Controllers/WeatherForecastController.cs
+++ AzureCICDAspCoreWebapi/Controllers/WeatherForecastController.cs
@@ -24,16 +24,9 @@ namespace AzureCICDAspCoreWebapi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "OM Namo Narayanaya, ON NamaSivayya, ON Namo Brahmaya";
         }
     }
 }
